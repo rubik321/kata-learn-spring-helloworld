93a218c4fc120355c0657c5709bf10c13ce96305
Danil <dvpudovkin@gmail.com>
2022-08-01 13:38:03 +0500

    info: complete task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println("Is bean2 the same as bean?: " + (bean == bean2));
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Is catBean2 the same as catBean1?: " + (catBean1 == catBean2));
     }
 }
